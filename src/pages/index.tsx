4e1dd68ac77d75a57960bdca9f8d3ceff80cd615
mzaini30 <muhzaini30@gmail.com>
2021-09-13 06:02:38 +0800

    oke

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import "./index.pcss"
+import {Link} from "react-router-dom"
+
+function Menu(){
+	interface Menunya {
+		judul: string
+		link: string
+	}
+
+	const menunya: Array<Menunya> = [
+		{judul: "Ngaji", link: "ngaji"}
+	]
+
+	for (let x of menunya){
+		import(`./${x.link}`)
+	}
+
+	return <>{menunya.map(x => <>
+		<Link to={`/${x.link}`}>{x.judul}</Link>
+	</>)}</>
+}
 
 export default function(){
 	document.title = 'Penjaga Quran'
 
 	return <div id='index'>
-		<h1>Penjaga Quran</h1>
+		<div className="text-center">
+			<h1>Penjaga Quran</h1>
+			<h2>ولقد يسرنا القرآن للذكر فهل من مدكر</h2>
+		</div>
+		<div className="menu">
+			<Menu/>
+		</div>
 	</div>
 }
---
+++ src/pages/ngaji.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function(){
+	document.title = "Ngaji"
+	
+	return <div id="ngaji">
+
+	</div>
+}
